89a93aafc1e43e2c798767f2d1863fd0007ac08f
Saphal Rimal <saphalrimal@gmail.com>
2025-05-15 14:38:08 +0545

    Changed cicd.yml to run on pushing to development branch

--- app/core/config.py
+++ app/core/config.py
@@ -4,14 +4,14 @@ from pydantic import ConfigDict
 
 class Settings(BaseSettings):
     debug: bool = True
-    environment: str
-    port: int
-    host: str
-    db_user: str
-    db_password: str
-    db_name: str
-    db_host: str
-    db_port: str
+    environment: str = "development"
+    port: int = 8000
+    host: str = "0.0.0.0"
+    db_user: str = "postgres"
+    db_password: str = "postgres"
+    db_name: str = "todos"
+    db_host: str = "localhost"
+    db_port: str = "5432"
 
     model_config = ConfigDict(env_file=".env")
 
